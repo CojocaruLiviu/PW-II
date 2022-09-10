6db282483cb1ebf18abf72e66258404542a51ba3
CojocaruLiviu <Liviuss2000@mail.ru>
2022-09-10 22:36:54 +0300

    Initial commit

---
+++ first/node-express-sample-master/src/routes/index.js
@@ -0,0 +1,12 @@
+import { Express } from 'express'
+
+const Router = require('express')
+const router = new Router()
+
+const modules = ['countries', 'regions', 'locations']
+
+modules.forEach(module => {
+    router.use(`/${module}`, require(`./${module}`))
+})
+
+module.exports = router
