5cc7fd8a31dcfac6670d63d165ca9f2a801edf82
CojocaruLiviu <Liviuss2000@mail.ru>
2022-09-11 13:05:50 +0300

    Countri update

--- Ex/ExpressEx1/src/controllers/CountriesController.js
+++ Ex/ExpressEx1/src/controllers/CountriesController.js
@@ -31,6 +31,12 @@ class CountriesController{
         return response.json(country);
     }
 
+    update(request, response){
+        const country = request.body;
+        country.id = request.params.id;
+        return response.json(country);
+    }
+
 }
 
 module.exports = new CountriesController()
--- Ex/ExpressEx1/src/routes/countries.js
+++ Ex/ExpressEx1/src/routes/countries.js
@@ -7,4 +7,7 @@ const controller = require('../controllers/CountriesController')
 router.get('/', controller.getAll)
 router.get('/:id', controller.getOne)
 router.post('/', controller.create)
+router.patch('/:id', controller.update)
+router.delete('/:id', controller.delete);
+
 module.exports = router
