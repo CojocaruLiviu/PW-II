65fb9b7b39d9f0e155729b681780162c1a944b29
CojocaruLiviu <Liviuss2000@mail.ru>
2022-09-11 14:08:37 +0300

    Add regions (Get/Post/Patch/Delete)

--- Ex/ExpressEx1/src/routes/index.js
+++ Ex/ExpressEx1/src/routes/index.js
@@ -2,7 +2,7 @@
 const Router = require('express')
 const router = new Router()
 
-const modules = ['countries']
+const modules = ['countries','regions']
 
 modules.forEach(module => {
     router.use(`/${module}`, require(`./${module}`))
